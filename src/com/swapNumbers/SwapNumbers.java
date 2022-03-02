6df151484ce1ec4fca44dc7a5657556564f011d3
smbiradar <smbiradar92@gmail.com>
2022-03-02 22:35:51 +0530

    Adding 2D Array_2.3.22

---
+++ src/com/swapNumbers/SwapNumbers.java
@@ -0,0 +1,25 @@
+package com.swapNumbers;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+
+	static int x, y, t;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Value of X");
+		x = sc.nextInt();
+		System.out.println("Enter the Value of Y");
+		y = sc.nextInt();
+
+		System.out.println("The numbers before Swapping are: X = " + x + " and Y = " + y);
+
+		t = x;
+		x = y;
+		y = t;
+
+		System.out.println("The numbers after Swapping are: X = " + x + " and Y = " + y);
+		sc.close();
+	}
+}
--- src/com/twoDArray/TwoDArray.java
+++ src/com/twoDArray/TwoDArray.java
@@ -1,7 +1,63 @@
 package com.twoDArray;
 
+import java.io.PrintWriter;
+
+import java.util.Scanner;
+
 public class TwoDArray {
 
-	int[] [] number;
-	
+	int[][] number;
+	int i, j;
+
+	public TwoDArray(int row, int col) {
+		number = new int[row][col];
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number of rows");
+		int row = sc.nextInt();
+
+		System.out.println("Enter the number of columns");
+		int column = sc.nextInt();
+
+		TwoDArray twoDArray = new TwoDArray(row, column);
+		twoDArray.saveNumbers(row, column);
+
+		System.out.println("----------Printing an Array to file-------");
+		twoDArray.printArray(row, column);
+
+		sc.close();
+	}
+
+	private void printArray(int row, int column) {
+		try {
+			PrintWriter writer = new PrintWriter("abc.txt");
+			for (i = 0; i < row; i++) {
+				for (j = 0; j < column; j++) {
+					writer.print(number[i][j]);
+				}
+				writer.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void saveNumbers(int row, int column) {
+		Scanner sc = new Scanner(System.in);
+		for (i = 0; i < row; i++) {
+			for (j = 0; j < column; j++) {
+
+				System.out.println("Enter the [" + i + "]" + "[" + j + "] :");
+				number[i][j] = sc.nextInt();
+			}
+		}
+
+		sc.close();
+	}
+
 }
